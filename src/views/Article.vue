1bd06f98414c63b0ea98b72fb32940a6ef5377f3
dobromir-hristov <dobromir92@gmail.com>
2019-10-11 23:00:37 +0300

    fix: fix Article visual style

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -1,25 +1,34 @@
 <template>
   <div class="Article">
+    <a
+      href="#"
+      data-testid="goBackButton"
+      @click.prevent="$router.go(-1)"
+    >
+      Go Back
+    </a>
     <transition mode="out-in" name="fade">
-      <div class="Article__content" v-if="!isLoading">
-        <div class="Article__rating mb-4">
-          <span class="font-bold text-grey">Rating:</span>
-          <span data-testid="articleRating">{{ article.rating }}</span> / 5
+      <div class="Article__content flex mt-32" v-if="!isLoading">
+        <div class="Article__image px-4 mb-4 w-1/3" v-if="article.image">
+          <img data-testid="articleImage" :src="article.image" class="shadow" alt="">
         </div>
-        <div class="Article__image mb-4" v-if="article.image">
-          <img data-testid="articleImage" :src="article.image" class="h-64 shadow" alt="">
-        </div>
-        <div data-testid="articleTitle" class="Article__title mb-4 text-3xl">
-          <span class="font-bold">Title:</span> {{ article.title }}
-        </div>
-        <div data-testid="articleAuthor" class="Article__author text-grey uppercase text-sm mb-4">
-          <span class="font-bold">Author:</span> {{ article.author }}
-        </div>
-        <div class="Article__body" data-testid="articleBody">
-          {{ article.body }}
+        <div class="w-2/3 text-left px-4">
+          <div data-testid="articleTitle" class="Article__title mb-4 text-3xl">
+            <span class="font-bold">Title:</span> {{ article.title }}
+          </div>
+          <div data-testid="articleAuthor" class="Article__author text-grey uppercase text-sm mb-4">
+            <span class="font-bold">Author:</span> {{ article.author }}
+          </div>
+          <div class="Article__rating mb-4">
+            <span class="font-bold text-grey mr-2">Rating:</span>
+            <span data-testid="articleRating">{{ article.rating }}</span> / 5
+          </div>
+          <div class="Article__body" data-testid="articleBody">
+            {{ article.body }}
+          </div>
         </div>
       </div>
-      <div class="can-load loading" v-else/>
+      <div class="can-load loading" v-else />
     </transition>
   </div>
 </template>
